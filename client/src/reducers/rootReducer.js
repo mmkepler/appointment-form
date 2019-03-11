4739563bc05b0b66fc7ef6aff61c63477c4f3921
Melissa Kepler <mmkepler@yahoo.com>
2019-03-11 00:43:46 -0400

    fix: removed unused redux-form

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import appointmentsReducer from './appointmentsReducer';
-import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
   appointmentsReducer: appointmentsReducer,
-  form: formReducer
 });
 
 export default rootReducer;
